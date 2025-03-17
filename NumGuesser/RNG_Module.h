c5df09c8089ba0d0dbca1d34fd374e8bcde2c6fe
Mohamed Al-Husainawi <Malhusainawi6279@conestogac.on.ca>
2025-03-17 18:36:26 -0400

    Added a header file for RNG module with function prototypes

---
+++ NumGuesser/RNG_Module.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef RNG_MODULE_H
+#define RNG_MODULE_H
+
+// Function to initialize RNG (seeding with time)
+void init_rng();
+
+// Function to generate random number between 1 and 100
+int generate_random_number();
+
+#endif

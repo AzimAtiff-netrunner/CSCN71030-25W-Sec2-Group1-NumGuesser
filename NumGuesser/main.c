18e0c50c1a80cbd7252bffadd833b75249f54841
Mohamed Al-Husainawi <Malhusainawi6279@conestogac.on.ca>
2025-03-17 18:41:25 -0400

    I created a temporary main.c file for testing RNG module functionality.

--- NumGuesser/main.c
+++ NumGuesser/main.c
@@ -1,9 +1,19 @@
 //NumGuesser By: Azim Atif, Jaden Mardini, Mohamed .H 
 
 #include <stdio.h>
-
+#include "rng_module.h"
 int main(int argc, char* argv) {//REQ-SYS-020
+    init_rng();  // Initialize RNG (seed once)
 
+    // Generate random number
+    int random_num = generate_random_number();
+
+    // Display the generated number
+    printf("Random Number Generated: %d\n", random_num);
 
 	return 0;
 }
+
+
+
+
